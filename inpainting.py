c7330f20f89e0d333ef5a9ec1d77fa371e8f9a4e
Sylvain Sénéchal <senechalsylvain@hotmail.com>
2019-04-26 06:48:56 +0200

    getDico and missing patches

--- inpainting.py
+++ inpainting.py
@@ -71,7 +71,7 @@ def readImage(path):
 def noise(img, percent):
     for row in range (len(img)):
         for col in range (len(image[row])):
-            if (random()*100 > percent):
+            if (random()*100 < percent):
                 for rgb in range (3):
                     img[row][col][rgb] = 0 # TODO voir si 0 est bien pour pixel retiré, ou plutot utiliser -100
     return img
@@ -88,7 +88,7 @@ def getPatch(img, row, col, h):
     patch = np.empty(shape=(h, h, 3))
     for i in range (h):
         for j in range (h):
-            print(img[row - int (h/2) + i][col - int (h/2) + j])
+            #print(img[row - int (h/2) + i][col - int (h/2) + j])
             patch[i][j] = img[row - int (h/2) + i][col - int (h/2) + j]
     return patch
 
@@ -96,7 +96,72 @@ def patchToVector(patch):
     return patch.flatten()
 def vectorToPatch(vector, h):
     return vector.reshape(h, h, 3)
+
+def getDictionary(img, h):
+    dictionary = []
+   
+    for row in range (int (h/2), len(img) - int (h/2)):
+        for col in range (int (h/2), len(img[row]) - int (h/2)):
+            dictionary.append( getPatch(img, row, col, h) )
+    dictionary = np.array(dictionary)
+    # TODO : A opti..
+    #dictionary = dictionary[np.where()]
+    #dictionary = filter(lambda patch: for row in range (h): for col in range (h): for rgb in range (3): patch[row][col][rgb] != 0, dictionary)
+    filteredDico = []
+    for pi in range (len(dictionary)): # A Clean....
+        remove = False
+        patch = dictionary[pi]
+        for row in range (len(patch)):
+            for col in range (len(patch[row])):
+                if patch[row][col][0] == 0 and patch[row][col][1] == 0 and patch[row][col][2] == 0:
+                    remove = True
+        if not remove:
+            filteredDico.append(patch)
+    return np.array(filteredDico)
+
+def getPatchMissingPixels(img, h):
+    patchMissingPx = []
+   
+    for row in range (int (h/2), len(img) - int (h/2)):
+        for col in range (int (h/2), len(img[row]) - int (h/2)):
+            patchMissingPx.append( getPatch(img, row, col, h) )
+    patchMissingPx = np.array(patchMissingPx)
+    # TODO : A opti..
+    #dictionary = dictionary[np.where()]
+    #dictionary = filter(lambda patch: for row in range (h): for col in range (h): for rgb in range (3): patch[row][col][rgb] != 0, dictionary)
+    filteredMissingPx = []
+    for pi in range (len(patchMissingPx)): # A Clean....
+        missingPx = False
+        patch = patchMissingPx[pi]
+        for row in range (len(patch)):
+            for col in range (len(patch[row])):
+                if patch[row][col][0] == 0 and patch[row][col][1] == 0 and patch[row][col][2] == 0:
+                    missingPx = True
+        if missingPx:
+            filteredMissingPx.append(patch)
+    return np.array(filteredMissingPx)
+
+def getMiddleDico(dico, h):
+    middle = []
+    for patch in range (len(dico)):
+        middle.append( dico[patch][int (h/2)][int (h/2)][0] )
+    return np.array(middle)
+
+def lassoImpaiting(dictionnary, middleDictionnary):
+    print("STARTING LASSO IMPAINTING")
+    lasso = Lasso(alpha=0.1)
+    lasso.fit(dictionnary, middleDictionnary)
+    
+    #predictions = lasso.predict(teX)
+    #print(lasso.score(teX, teY))
+    #print(lasso.intercept_)
+    #print("score lassoRegression : ", score(predictions, teY), "out of : ", len(teY))
+    #print(lasso.coef_)
+
+
 if __name__=="__main__":
+    ### PART 1 ################################################################
+
     # Loading data
     dataTrain = load_usps("USPS_train.txt")
     trainX = dataTrain[0]
@@ -141,20 +206,27 @@ if __name__=="__main__":
 
 #0 -> 127 -1,0, 128 => 255 : 0, 1
     
-    # PART 2
+    ### PART 2 ################################################################
     # TODO : voir si les fonctions de noise et de manipulation d'image modifient l'image d'origine (à priori oui)
-    image = readImage("imgTest.jpg")
+    image = readImage("akita.jpg")
     #plt.figure()
     #plt.imshow(image)
-    #plt.figure()
-    #plt.imshow(noise(image, 50))
+    plt.figure()
+    plt.imshow(noise(image, 5))
     #plt.figure()
     #plt.imshow(deleteRectangle(image, 100, 200, 60, 120))
     
     patch = getPatch(image, 1, 2, 3)
-    print(patch)
-    print(patchToVector(patch))
-    print( vectorToPatch(patchToVector(patch), 3) )
+    #print(patch)
+    #print(patchToVector(patch))
+    #print( vectorToPatch(patchToVector(patch), 3) )
     
+    dico = getDictionary(image, 3)
+    #patchMissingPx = getPatchMissingPixels(image, 3)
+    middleDico = getMiddleDico(dico, 3)
+    #print(middleDico)
     
+    lassoImpaiting(dico, middleDico)
+   
     # TODO idee : faire une fonction de dst entre imgage de base et image reconstruite pour voir la qualite
+    # TODO idee : noise function avec perlin noise ?
